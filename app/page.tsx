a56e67e16d63684602c92bd071665571ab89e0c3
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2026-08-27 17:28:10 +0200

    feat: enable orgs

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { UserButton } from "@clerk/nextjs"
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs"
 import { auth } from "@clerk/nextjs/server"
 import Image from "next/image"
 
@@ -28,6 +28,7 @@ export default async function Page() {
         </EmptyHeader>
       </Empty>
       <UserButton />
+      <OrganizationSwitcher />
     </div>
   )
 }
